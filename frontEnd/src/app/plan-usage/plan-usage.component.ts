edbf7dd1eb2a8524e0c59bf0ed71fb413276fa2f
bernardocjs <bernardocjs@gmail.com>
2023-05-18 12:51:15 -0300

    feat: :sparkles: adds basic plan screen structure

---
+++ frontEnd/src/app/plan-usage/plan-usage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-usage',
+  templateUrl: './plan-usage.component.html',
+  styleUrls: ['./plan-usage.component.scss']
+})
+export class PlanUsageComponent {
+
+}
---
+++ frontEnd/src/app/plan-usage/plan-usage.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlanUsageComponent } from './plan-usage.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlanUsageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlanUsageModule { }
